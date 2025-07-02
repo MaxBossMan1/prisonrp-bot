3135a896d5f1e6af808c4cd0733c456599872553
DigitalDeltaGaming <contact@digitaldeltagaming.net>
2025-07-02 17:34:18 -0400

    Fix bot status using proper Discord.js v14 ActivityType enum

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,4 @@
-const { Events } = require('discord.js');
+const { Events, ActivityType } = require('discord.js');
 
 module.exports = {
     name: Events.ClientReady,
@@ -11,7 +11,7 @@ module.exports = {
         logger.info(`Bot is in ${client.guilds.cache.size} guilds`);
         
         // Set bot status
-        client.user.setActivity('DM for support', { type: 'WATCHING' });
+        client.user.setActivity('DM for support', { type: ActivityType.Watching });
         
         // Clean old sessions and logs on startup
         try {
